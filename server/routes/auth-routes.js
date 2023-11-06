71efa19d547c1fa059f9917ef62946959534de88
VithalAgrawal <vithalagrawal6@gmail.com>
2023-11-07 00:37:40 +0530

    Added routes
    Added auth, admin, user and news admin routes.

--- server/routes/auth-routes.js
+++ server/routes/auth-routes.js
@@ -3,15 +3,19 @@ const express = require('express');
 const router = express.Router();
 
 const authControllers = require('../controllers/auth-controller');
+const refreshTokenController = require('../controllers/refresh-token-controller');
 
 // /signup => POST
-router.post('/signup', authControllers.signup);
+router.post('/signup', authControllers.signup); //create a new user
 
 // /login => POST
-router.post('/login', authControllers.login);
+router.post('/login', authControllers.login); //login a user
 
 // /logout => POST
-router.post('/logout', authControllers.logout);
+router.post('/logout', authControllers.logout); //logout a user
+
+// /refresh => POST
+router.post('/refresh', refreshTokenController.handleRefreshToken);
 
 
 module.exports = router;
