db8a5907be82f9a84ece9ca5425a91c0a918ab37
PRITISH RANJAN PANDA <85501658+pandapritish25@users.noreply.github.com>
2023-11-09 18:27:06 +0530

    Add files via upload

---
+++ gcp/app_engine.bash
@@ -0,0 +1,19 @@
+
+### App Engine
+
+```
+cd default-service
+gcloud app deploy
+gcloud app services list
+gcloud app versions list
+gcloud app instances list
+gcloud app deploy --version=v2
+gcloud app versions list
+gcloud app browse
+gcloud app browse --version 20210215t072907
+gcloud app deploy --version=v3 --no-promote
+gcloud app browse --version v3
+
+```
+
+### Kubernetes
