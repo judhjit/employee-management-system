5258ac1fb4492d7a2f3dde92ca341527f519fce3
PRITISH RANJAN PANDA <85501658+pandapritish25@users.noreply.github.com>
2023-11-16 16:30:16 +0530

    Add files via upload

---
+++ my-react-app/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      hello world and hello people of xyz group i am pritish
+    </div>
+  );
+}
+
+export default App;
