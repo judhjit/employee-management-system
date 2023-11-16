4a034bcd39c01fc0a602a6a50997e1a3b7e90ea2
PRITISH RANJAN PANDA <85501658+pandapritish25@users.noreply.github.com>
2023-11-16 16:50:29 +0530

    Delete gcp directory

--- gcp/startup_script.bash
+++
@@ -1,6 +0,0 @@
-#! apache is a long software that take huge long time to download so because of that we cant install it again and again thus direct apache installation could be there
-
-#!/bin/bash
-apt update 
-apt -y install apache2
-echo "Hello world from $(hostname) $(hostname -I)" > /var/www/html/index.html
